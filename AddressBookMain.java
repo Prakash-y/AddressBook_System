5301cba81695b8730bdb8c4b9e8112e41505c73f
Prakash <py64886@gmail.com>
2023-07-12 11:15:12 +0530

    Added: UC11 Sort by name

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -3,12 +3,14 @@ package AddressBookSystem;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class AddressBookMain {
+public class AddressBookMain
+{
     HashMap<String, ArrayList<Contact>> addressBooks = new HashMap<>();
 
     Scanner input = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Welcome to AddressBook program....!!!!");
         Scanner input = new Scanner(System.in);
         AddressBookMain addressBookMain = new AddressBookMain();
@@ -19,8 +21,9 @@ public class AddressBookMain {
             System.out.println("Enter what you want to perform");
             System.out.println("Press 1 to create new address book" + '\n' + "Press 2 to perform operation " +
                     "on existing address book" + '\n' + "Press 3 to search contacts with city " + '\n' + "Press 4 to get person with city" +
-                    '\n' + "Press 5 to get number of contacts by city" + '\n' + "Press 0 to exit");
-            final int createAddressBook = 1, operateExisting = 2, searchContacts = 3, getPersonWithCity = 4, getNoOfContactByCity = 5, exit = 0;
+                    '\n' + "Press 5 to get number of contacts by city" + '\n' + "Press 6 to get sorted contacts by name" + '\n' + "Press 0 to exit");
+            final int createAddressBook = 1, operateExisting = 2, searchContacts = 3, getPersonWithCity = 4, getNoOfContactByCity = 5,
+                      sortedContactsByName = 6, exit = 0;
             int option = input.nextInt();
             switch (option) {
                 case createAddressBook:
@@ -32,7 +35,7 @@ public class AddressBookMain {
                     if (addressBookMain.addressBooks.containsKey(inputKey))
                         addressBook.callAddressBook(addressBookMain.addressBooks.get(inputKey));
                     else
-                        System.out.println("Entered key address book not available");
+                        System.out.println("Entered book address book not available");
                     break;
                 case searchContacts:
                     addressBookMain.searchContactsWithCity();
@@ -43,6 +46,9 @@ public class AddressBookMain {
                 case getNoOfContactByCity:
                     addressBookMain.getNumberContacts();
                     break;
+                case sortedContactsByName:
+                    addressBookMain.getSortedContactByName();
+                    break;
                 case exit:
                     loop = false;
                     break;
@@ -52,25 +58,29 @@ public class AddressBookMain {
         }
     }
 
-    public void createAddressBook() {
+    public void createAddressBook() 
+    {
         System.out.println("Create address book of your choice");
         ArrayList<Contact> contacts = new ArrayList<>();
-        System.out.println("Enter unique key name");
+        System.out.println("Enter unique book name");
         String name = input.next();
         if (!addressBooks.containsKey(name))
             addressBooks.put(name, contacts);
         else
-            System.out.println("Entered key is already available");
+            System.out.println("Entered book is already available");
     }
 
-    public void searchContactsWithCity() {
+    public void searchContactsWithCity()
+    {
         System.out.println("Please enter city name");
         String cityName = input.next();
-        List<Contact> listOfContacts = addressBooks.values().stream().flatMap(p -> p.stream()).filter(p -> p.getCity().equalsIgnoreCase(cityName)).collect(Collectors.toList());
+        List<Contact> listOfContacts = addressBooks.values().stream().flatMap(p -> p.stream())
+                                       .filter(p -> p.getCity().equalsIgnoreCase(cityName)).collect(Collectors.toList());
         System.out.println(listOfContacts);
     }
 
-    public void getContactByCityAndState() {
+    public void getContactByCityAndState() 
+    {
         List<Contact> myContactList = addressBooks.values().stream().flatMap(A -> A.stream()).collect(Collectors.toList());
         Map<String, List<Contact>> myContactListByCity = myContactList.stream().collect(Collectors.groupingBy(Contact::getCity));
         System.out.println(myContactListByCity);
@@ -78,11 +88,18 @@ public class AddressBookMain {
         System.out.println(myContactListByState);
     }
 
-    public void getNumberContacts() {
+    public void getNumberContacts() 
+    {
         System.out.println("Please enter city name");
         String cityName = input.next();
         long count = addressBooks.values().stream().flatMap(p -> p.stream()).filter(p -> p.getCity().equalsIgnoreCase(cityName)).count();
         System.out.println("Count of contacts with " + cityName + " are " + count);
     }
 
+    public void getSortedContactByName() 
+    {
+        List<Contact> nameSortedAddressBook = addressBooks.values().stream().flatMap(A -> A.stream())
+                .sorted((Comparator.comparing(Contact::getFirstName))).collect(Collectors.toList());
+        System.out.println(nameSortedAddressBook);
+    }
 }
